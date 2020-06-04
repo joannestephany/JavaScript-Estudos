79d26343c8a0b0d6465beda0b440195cdfc6711d
Joanne Stephany <58868281+joannestephany@users.noreply.github.com>
2020-06-04 18:23:15 -0300

    Add files via upload

---
+++ Testes/interação usuário/script.js
@@ -0,0 +1,7 @@
+function perguntas() {
+    var nome = window.prompt('Qual é o seu nome?')
+
+    var idade = window.prompt('Olá ' + nome + '! Quantos anos você tem?')
+
+    window.alert(`Olá ${nome }, que tem  ${idade} anos de idade! Uau`)
+}
---
+++ Testes/teste botões/scripts.js
@@ -0,0 +1,9 @@
+function funcao1() {
+    window.alert('Beba água')
+}
+function funcao2() {
+    window.alert('pratique exercicios')
+}
+function funcao3() {
+    window.alert('fique em casa')
+}
